a6552a84c0eb3253ec8a80b4c8501fa9c238cbe5
JaegeonJo <jaegeon.dev@gmail.com>
2024-03-25 18:33:34 +0900

    임시 포맷팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,73 @@
-import { useState, PropsWithChildren, Children } from 'react';
+import { useState, PropsWithChildren, Children } from "react";
 
 type TodoItem = {
-  key: number,
-  content: string,
-  completed: boolean,
+  key: number;
+  content: string;
+  completed: boolean;
 };
 
 type TodoItemProps = PropsWithChildren<{
-  item: TodoItem,
-  onChangeCheckbox: (checked: boolean) => void,
-  onClickRemove: () => void,
-  onChangeContent: (content: string) => void,
+  item: TodoItem;
+  onChangeCheckbox: (checked: boolean) => void;
+  onClickRemove: () => void;
+  onChangeContent: (content: string) => void;
 }>;
 
-function TodoItemComponent({ item, onChangeCheckbox, onClickRemove, onChangeContent, children }: TodoItemProps) {
-  const [ isEdit, setIsEdit ] = useState(false);
+function TodoItemComponent({
+  item,
+  onChangeCheckbox,
+  onClickRemove,
+  onChangeContent,
+  children,
+}: TodoItemProps) {
+  const [isEdit, setIsEdit] = useState(false);
   return (
     <div>
-      <input type='checkbox' checked={ item.completed } 
-        onChange={ ({ target }) => {onChangeCheckbox(target.checked);}}/>
-      { 
-        isEdit ? 
+      <input
+        type="checkbox"
+        checked={item.completed}
+        onChange={({ target }) => {
+          onChangeCheckbox(target.checked);
+        }}
+      />
+      {isEdit ? (
         <span>
-          <input type="text" value={ item.content } onChange={(e) => {onChangeContent(e.target.value);}} />
-          <button onClick={ () => {setIsEdit(false);} } >Update</button> 
-        </span> :
-        <span>
-          { item.content }
-          <button onClick={ () => {setIsEdit(true);} } >Edit</button> 
+          <input
+            type="text"
+            value={item.content}
+            onChange={(e) => {
+              onChangeContent(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              setIsEdit(false);
+            }}
+          >
+            Update
+          </button>
         </span>
-      }
-      <button onClick={ onClickRemove } >Remove</button>
-      { children }
+      ) : (
+        <span>
+          {item.content}
+          <button
+            onClick={() => {
+              setIsEdit(true);
+            }}
+          >
+            Edit
+          </button>
+        </span>
+      )}
+      <button onClick={onClickRemove}>Remove</button>
+      {children}
     </div>
   );
 }
 
 function App() {
-  const [ todoList, setTodoList ] = useState<TodoItem[]>([]);
-  const [ inputContent, setInputContent ] = useState<string>('');
+  const [todoList, setTodoList] = useState<TodoItem[]>([]);
+  const [inputContent, setInputContent] = useState<string>("");
   const newTodoItem: TodoItem = {
     key: todoList.length,
     content: inputContent,
@@ -47,73 +76,74 @@ function App() {
   return (
     <div>
       <label>
-        Todo: 
-        <input type="text" value={inputContent} 
-          onChange={(event)=>{setInputContent(event.target.value);}}/> 
+        Todo:
+        <input
+          type="text"
+          value={inputContent}
+          onChange={(event) => {
+            setInputContent(event.target.value);
+          }}
+        />
       </label>
-      <button 
-        onClick={
-          () => {
-            if (inputContent !== '') {
-              setTodoList(() => {
-                return [...todoList, newTodoItem];
-              });
-              setInputContent('');
-            }
+      <button
+        onClick={() => {
+          if (inputContent !== "") {
+            setTodoList(() => {
+              return [...todoList, newTodoItem];
+            });
+            setInputContent("");
           }
-        }>Add</button>
+        }}
+      >
+        Add
+      </button>
       <div>
-        { 
-          todoList.map((item, itemIdx) => {
-            return (<TodoItemComponent 
-                      key={ item.key }
-                      item={ item } 
-                      onChangeCheckbox={ 
-                        (checked) => {
-                          setTodoList( () => { 
-                            return todoList.map((value, index) => { 
-                              if (itemIdx === index ) {
-                                return {
-                                  key: value.key,
-                                  content: value.content,
-                                  completed: checked,
-                                };
-                              }
-                              return value;
-                            });
-                          });
-                        }}
-                      onClickRemove={
-                        () => {
-                          setTodoList( () => {
-                            return todoList.filter((_, index) => {
-                              if (itemIdx === index) {
-                                return false;
-                              }
-                              return true;
-                            });
-                          });
-                        }
-                      }
-                      onChangeContent={
-                        (content) => {
-                          setTodoList( () => {
-                            return todoList.map((value, index) => {
-                              if (itemIdx === index) {
-                                return {
-                                  key: value.key,
-                                  content: content,
-                                  completed: value.completed,
-                                };
-                              }
-                              return value;
-                            });
-                          });
-                        }
-                      }
-                    />);
-          })
-        }
+        {todoList.map((item, itemIdx) => {
+          return (
+            <TodoItemComponent
+              key={item.key}
+              item={item}
+              onChangeCheckbox={(checked) => {
+                setTodoList(() => {
+                  return todoList.map((value, index) => {
+                    if (itemIdx === index) {
+                      return {
+                        key: value.key,
+                        content: value.content,
+                        completed: checked,
+                      };
+                    }
+                    return value;
+                  });
+                });
+              }}
+              onClickRemove={() => {
+                setTodoList(() => {
+                  return todoList.filter((_, index) => {
+                    if (itemIdx === index) {
+                      return false;
+                    }
+                    return true;
+                  });
+                });
+              }}
+              onChangeContent={(content) => {
+                setTodoList(() => {
+                  return todoList.map((value, index) => {
+                    if (itemIdx === index) {
+                      return {
+                        key: value.key,
+                        content: content,
+                        completed: value.completed,
+                      };
+                    }
+                    return value;
+                  });
+                });
+              }}
+            />
+          );
+        })}
       </div>
     </div>
   );
